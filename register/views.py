764fa266dd39b7e173bbb8fa633a2097e099ebec
Matthew Early <matthewjearly@gmail.com>
2017-01-15 15:42:42 -0500

    testing out django

---
+++ register/views.py
@@ -0,0 +1,42 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Link
+from .models import File
+
+def index(request):
+    all_links = Link.objects.all() 
+    html = ''
+    for link in all_links:
+        html += '<a href = "' + link.link + '">' + link.title + '</a><br>'
+    return HttpResponse(html)
+
+def detail(request, fid):
+    file = File.objects.get(id=fid)
+    template = loader.get_template('register/project.html')
+    context = {
+        'file': file,
+    }
+    #html = file.title + '<br>' + file.author + '<br>' + file.location + '<br>' + file.rating + '<br>' + file.type + '<br><br>' + file.description + '<br>'
+    return HttpResponse(template.render(context, request))
+
+def about(request):
+    template = loader.get_template('register/about.html')
+    context = {
+        'file': file,
+    }
+    return HttpResponse(template.render(context, request))
+
+def login(request):
+    template = loader.get_template('register/login.html')
+    context = {
+        'file': file,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def register(request):
+    template = loader.get_template('register/register.html')
+    context = {
+        'file': file,
+    }
+    return HttpResponse(template.render(context, request))
